3597112a6a0b2f92ec4f7dd444306ad9ee58d6da
stephanpaquet <stephanpaquet@gmail.com>
2017-06-23 08:42:45 -0400

    Part 1

---
+++ readme.md
@@ -0,0 +1,7 @@
+### sh: 1: cross-env: not found
+j'ai eu cet erreur lorsque j'executait npm run dev
+```
+sudo n latest
+
+
+```
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Chat room</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Chatroom</h1>
+
+		<div id="app">
+			<example>
+			</example>
+		</div>
+	</div>
+
+	<script src="/js/app.js"></script>
+</body>
+</html>
